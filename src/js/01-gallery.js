0dc84a52169c571980d50cc7489c33b3e3b4628c
Chalaya22 <chalayaolga22@gmail.com>
2023-08-22 15:47:10 +0300

    step 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,20 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
 
 console.log(galleryItems);
+// import { add as fnAdd, mult } from './02-video';
+// const add = 5;
+// console.log(fnAdd(4, 5));
+// console.log(add);
+// console.log(mult(3, 5));
+
+// якщо треба імпортувати все, то тоді шснує такий синтаксис:
+// import * as data from './02-video';
+// console.log(data); //це вже буде обьєкт з імпортом
+// console.log(data.add(4, 5)); // 109 достукатись до потрібноі функціі
+// import value from './02-video';//це імпортується дефольний експорт
+// console.log(value); //60
